a7779bea974401f18ae5aa49aa803dee6862a671
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-10-31 16:13:45 +0300

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,7 +1,8 @@
 public class Customer {
     private String username;
     private String password;
-
+    private String name;
+    private int age;
 
     public Customer(String username, String password) {
         this.username = username;
@@ -10,5 +11,33 @@ public class Customer {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
